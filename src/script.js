7f63819fa0aeb580a443c81d9d3bed7a07aa70fb
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2023-12-16 01:44:26 +0700

    edit file script.js and app.js

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
-//import p from './utils.js';
+import p from './utils.js';
 import sum from './index.js';
 import './css/style.css';
 import './scss/style.scss'
 
-// p().then((res) => {
-//     console.log(res);
-// })
+p().then((res) => {
+    console.log(res);
+})
 
 console.log('Total : ', sum(2, 7));
 
