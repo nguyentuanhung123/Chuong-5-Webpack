f996b791a75de8ca558db53b9be95918b40564a6
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2023-12-17 01:22:01 +0700

    bai 24 : Source-map , edit file webpack.config and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -6,42 +6,46 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // console.log('path.resolve() : ', path.resolve()); --> path.resolve() :  E:\document\webpack
 // console.log(`path.resolve(__dirname, 'dist') : `, path.resolve(__dirname, 'dist')); --> path.resolve(__dirname, 'dist') :  E:\document\webpack\dist
 
-module.exports = {
-    mode: 'production',
-    entry: {
-        app: path.resolve('src/script.js') //file đầu vào
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js', // name = app , contenthash = mã bất kì , file đầu ra
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss|css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] // file app.css được sinh ra lúc ban đầu
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css' // file app.123....css và app.123....js được sinh ra để thể hiện 1 chức năng mới
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Webpack App', // tên tiêu đề phải đc đặt trong template
-            filename: 'index.html', //tên file được sinh ra
-            template: 'src/template.html' //file mẫu được gắn vào khi index.html được sinh ra
-        })
-    ],
-    devServer: {
-        static: {
-            directory: 'dist' // Đường dẫn tương đối đến với thư mục chứa index.html
+module.exports = (env) => {
+    const isDevelopment = Boolean(env.development);
+    return {
+        mode: isDevelopment ? 'development' : 'production',
+        entry: {
+            app: path.resolve('src/script.js') //file đầu vào
         },
-        port: 3000, // Port thay cho port mặc định
-        open: true, // Mở trang webpack khi chạy terminal
-        hot: true, // Bật tính năng reload nhanh Hot Module Replacement
-        compress: true, // Bật Gzip cho các tài nguyên
-        historyApiFallback: true // Set true nếu bạn dùng cho các SPA và sử dụng History API của HTML5
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].[contenthash].js', // name = app , contenthash = mã bất kì , file đầu ra
+            clean: true // xoá những file cũ không cần thiết 
+        },
+        devtool: isDevelopment ? 'source-map' : false,
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss|css$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] // file app.css được sinh ra lúc ban đầu
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css' // file app.123....css và app.123....js được sinh ra để thể hiện 1 chức năng mới
+            }),
+            new HtmlWebpackPlugin({
+                title: 'Webpack App', // tên tiêu đề phải đc đặt trong template
+                filename: 'index.html', //tên file được sinh ra
+                template: 'src/template.html' //file mẫu được gắn vào khi index.html được sinh ra
+            })
+        ],
+        devServer: {
+            static: {
+                directory: 'dist' // Đường dẫn tương đối đến với thư mục chứa index.html
+            },
+            port: 3000, // Port thay cho port mặc định
+            open: true, // Mở trang webpack khi chạy terminal
+            hot: true, // Bật tính năng reload nhanh Hot Module Replacement
+            compress: true, // Bật Gzip cho các tài nguyên
+            historyApiFallback: true // Set true nếu bạn dùng cho các SPA và sử dụng History API của HTML5
+        }
     }
 }
